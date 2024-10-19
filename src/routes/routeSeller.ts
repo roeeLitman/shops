67c13b7c82e265c02209ce27085f11b7d6506cfc
roee ltmanovich <roeeli770@gmail.com>
2024-10-19 20:39:58 +0300

    create rout file for  Seller

---
+++ src/routes/routeSeller.ts
@@ -0,0 +1,69 @@
+import {Response, Router, Request} from "express";
+
+const rootSeller:Router = Router()
+
+// create seller
+rootSeller.post('/register', async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
+
+//get all order
+rootSeller.get('/', async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
+
+//geet specifi order
+rootSeller.get('/id', async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
+
+// get all order of specifi client
+rootSeller.get('/getorder/:id', async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
+
+// add product 
+rootSeller.put('/', async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
+
+// delete prodact
+rootSeller.delete('/', async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
+
+//delete shop
+rootSeller.delete('/', async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
+
+
+export default rootSeller
