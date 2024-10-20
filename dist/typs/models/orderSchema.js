a7566e1d75e2bfe3f2d054980158775b204d48d5
roee ltmanovich <roeeli770@gmail.com>
2024-10-20 17:33:01 +0300

    create schema for order

--- dist/typs/models/orderSchema.js
+++ dist/typs/models/orderSchema.js
@@ -1,2 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.orderSchema = void 0;
+const mongoose_1 = require("mongoose");
+exports.orderSchema = new mongoose_1.Schema({
+    project: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: "Project",
+        required: [true, "you dont enter id of object"]
+    },
+    send: {
+        type: Date,
+        required: [true, "enter time"]
+    },
+    arrived: {
+        type: Date,
+        required: false,
+    }
+});
--- src/typs/models/orderSchema.ts
+++ src/typs/models/orderSchema.ts
@@ -1,7 +1,24 @@
+import mongoose, { Schema } from "mongoose"
 import { IProject } from "./projectSchema"
 
 export interface IOrder {
-    project: IProject
+    project: mongoose.Types.ObjectId
     send: Date
     arrived: Date
 }
+
+export const orderSchema = new Schema<IOrder>({
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required:[true, "you dont enter id of object"]   
+    },
+    send: {
+        type: Date,
+        required:[true, "enter time"]   
+    },
+    arrived: {
+        type: Date,
+        required: false,  
+    }
+})
